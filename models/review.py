e9c4b74355dc391d73f073c8abfca89f49edb6d2
Waheed Khaled <waheedkhaled95@gmail.com>
2024-03-21 04:05:28 +0200

    Reviews

--- models/review.py
+++ models/review.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, ForeignKey, String
+import os
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """Review classto store review information"""
+    __tablename__ = 'reviews'
 
-    place_id = ""
-    user_id = ""
-    text = ""
+    place_id = Column(
+        String(60), ForeignKey('places.id'), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    user_id = Column(
+        String(60), ForeignKey('users.id'), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    text = Column(
+        String(1024), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
