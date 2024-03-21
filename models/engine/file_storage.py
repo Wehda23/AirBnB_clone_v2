de5064f6a7c56010482720a8bb253f5d709b29df
Waheed Khaled <waheedkhaled95@gmail.com>
2024-03-21 02:08:12 +0200

    Filtering Hot fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,7 +11,7 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls:
+        if cls is None:
             return FileStorage.__objects
         # Create a temporary objects holder
         data = {}
