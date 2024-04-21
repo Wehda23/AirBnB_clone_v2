76e039047ff696881a5f2e2855551927b570b3d0
Waheed Khaled <waheedkhaled95@gmail.com>
2024-04-21 20:47:05 +0200

    test

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+File contains the script to start a flask application
+"""
+from flask import Flask
+
+# Define application
+app: Flask = Flask(__name__)
+
+# Define route for the root url "/"
+@app.route("/", strict_slashes=False)
+def hello_hbnb() -> str:
+    """Displays A text message"""
+    return "Hello HBNB!"
+
+
+
+# Main
+if __name__ == "__main__":
+    # Start Flask Server
+    app.run(host='0.0.0.0', port=5000)
